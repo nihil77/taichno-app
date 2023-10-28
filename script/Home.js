f50b50208d7b8b29ce647e67a2aa8dc3048a4376
john <simonlegaspi27@gmail.com>
2023-10-28 22:23:33 +0800

    dev/20%

---
+++ script/Home.js
@@ -0,0 +1,65 @@
+// Select The Elements
+var toggle_btn;
+var big_wrapper;
+var hamburger_menu;
+
+function declare() {
+  toggle_btn = document.querySelector(".toggle-btn");
+  big_wrapper = document.querySelector(".big-wrapper");
+  hamburger_menu = document.querySelector(".hamburger-menu");
+}
+
+const main = document.querySelector("main");
+
+declare();
+
+let dark = false;
+
+function toggleAnimation() {
+  // Clone the wrapper
+  dark = !dark;
+  let clone = big_wrapper.cloneNode(true);
+  if (dark) {
+    clone.classList.remove("light");
+    clone.classList.add("dark");
+  } else {
+    clone.classList.remove("dark");
+    clone.classList.add("light");
+  }
+  clone.classList.add("copy");
+  main.appendChild(clone);
+
+  document.body.classList.add("stop-scrolling");
+
+  clone.addEventListener("animationend", () => {
+    document.body.classList.remove("stop-scrolling");
+    big_wrapper.remove();
+    clone.classList.remove("copy");
+    // Reset Variables
+    declare();
+    events();
+  });
+}
+
+function events() {
+  toggle_btn.addEventListener("click", toggleAnimation);
+  hamburger_menu.addEventListener("click", () => {
+    big_wrapper.classList.toggle("active");
+  });
+}
+
+events();
+
+// Get the link element by its ID
+const signupLink = document.getElementById('signupLink');
+
+// Add a click event listener to the link
+signupLink.addEventListener('click', function (event) {
+  event.preventDefault(); // Prevent the default behavior (navigating to the href)
+  
+  // Define the URL you want to navigate to
+  const url = 'signup.html'; // Replace with your desired URL
+
+  // Navigate to the specified URL
+  window.location.href = url;
+});
---
+++ script/signup.js
@@ -0,0 +1,142 @@
+const inputs = document.querySelectorAll(".input-field");
+const toggle_btn = document.querySelectorAll(".toggle");
+const main = document.querySelector("main");
+const bullets = document.querySelectorAll(".bullets span");
+const images = document.querySelectorAll(".image");
+
+inputs.forEach((inp) => {
+  inp.addEventListener("focus", () => {
+    inp.classList.add("active");
+  });
+  inp.addEventListener("blur", () => {
+    if (inp.value !== "") return;
+    inp.classList.remove("active");
+  });
+});
+
+toggle_btn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    main.classList.toggle("sign-up-mode");
+  });
+});
+
+function moveSlider() {
+  let index = this.dataset.value;
+
+  let currentImage = document.querySelector(`.img-${index}`);
+  images.forEach((img) => img.classList.remove("show"));
+  currentImage.classList.add("show");
+
+  const textSlider = document.querySelector(".text-group");
+  textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
+
+  bullets.forEach((bull) => bull.classList.remove("active"));
+  this.classList.add("active");
+}
+
+bullets.forEach((bullet) => {
+  bullet.addEventListener("click", moveSlider);
+});
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCnWbrjWukZRLmPF6uWqR4Wo_3m-rtz2hI",
+  authDomain: "web-tai-6ebaa.firebaseapp.com",
+  databaseURL: "https://web-tai-6ebaa-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "web-tai-6ebaa",
+  storageBucket: "web-tai-6ebaa.appspot.com",
+  messagingSenderId: "149766567228",
+  appId: "1:149766567228:web:202ecd644d35a3c251da39"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const auth = firebase.auth()
+
+// Function to handle displaying error messages
+function displayErrorMessage(message) {
+  const errorMessageElement = document.getElementById('error-message');
+  errorMessageElement.textContent = message;
+  errorMessageElement.style.display = 'block'; // Display the error message
+}
+
+// Function to clear the error message
+function clearErrorMessage() {
+  const errorMessageElement = document.getElementById('error-message');
+  errorMessageElement.textContent = '';
+  errorMessageElement.style.display = 'none'; // Hide the error message
+  errorMessageElement.style.color = 'red';
+}
+
+// Sign-in Form
+const signInForm = document.querySelector('.sign-in-form');
+const emailLoginInput = signInForm.querySelector('input[type="email"]');
+const passwordLoginInput = signInForm.querySelector('input[type="password"]');
+
+// Add event listeners to clear the error message when input fields receive focus
+emailLoginInput.addEventListener('focus', clearErrorMessage);
+passwordLoginInput.addEventListener('focus', clearErrorMessage);
+
+signInForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const email = emailLoginInput.value;
+  const password = passwordLoginInput.value;
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      console.log("User logged in:", userCredential.user);
+      window.location.href = "success.html";
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      displayErrorMessage("Invalid email or password. Please try again.");
+    });
+});
+
+// SIGN UP FORM
+
+// Function to handle displaying and hiding the error message
+function displayErrorSignUpMessage(message) {
+  const errorMessageElement = document.getElementById('error-message-signup');
+  errorMessageElement.textContent = message;
+  errorMessageElement.style.display = 'block'; // Show the error message
+}
+
+function hideErrorSignUpMessage() {
+  const errorMessageElement = document.getElementById('error-message-signup');
+  errorMessageElement.textContent = '';
+  errorMessageElement.style.display = 'none'; // Hide the error message
+  errorMessageElement.style.color = 'red';
+}
+
+// Sign-up Form
+const signUpForm = document.querySelector('.sign-up-form');
+const nameInput = signUpForm.querySelector('input[type="text"]');
+const emailInput = signUpForm.querySelector('input[type="email"]');
+const passwordInput = signUpForm.querySelector('input[type="password"]');
+
+signUpForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  hideErrorSignUpMessage(); // Clear any previous error message
+
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  auth.createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      console.log("User signed up:", userCredential.user);
+      userCredential.user.updateProfile({
+        displayName: name
+      });
+
+      // Redirect the user to the login page after successful sign-up
+      window.location.href = "signup.html"; // Replace with the actual login page URL
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      displayErrorSignUpMessage("An error occurred. Please try again later.");
+    });
+});
+
